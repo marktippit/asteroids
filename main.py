ff92a0df19fae04a3c6b789d4a1d60a80a186c5a
marktippit <marktippit@gmail.com>
2024-10-26 00:19:12 -0400

    added asteroids

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while(True):
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
-        
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        
-        #limit the framerate to 60 fps
-        dt = clock.tick(60)/1000
-        
 
-    print("Starting asteroids!")
-    print("Screen width: {}".format(SCREEN_WIDTH))
-    print("Screen height: {}".format(SCREEN_HEIGHT))
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.colliding(player):
+                print("Game over!")
+                sys.exit()
+                
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
